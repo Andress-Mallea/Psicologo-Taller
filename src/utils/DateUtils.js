e59319c7ff7bfefd62f1e3b72f90654bcfa04b8c
Andres Mallea <159574476+Andress-Mallea@users.noreply.github.com>
2026-03-19 14:47:36 -0400

    Delete src directory

--- src/utils/DateUtils.js
+++
@@ -1,8 +0,0 @@
-export const DateUtils = {
-    // Convierte el formato de FullCalendar al que acepta el input datetime-local
-    formatToInput(dateString) {
-        const date = new Date(dateString);
-        const tzOffset = date.getTimezoneOffset() * 60000;
-        return new Date(date - tzOffset).toISOString().slice(0, 16);
-    }
-};
