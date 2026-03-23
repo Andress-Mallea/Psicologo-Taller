9f1f30f856846677660dc3958b748d618ea26c15
Andres Mallea <159574476+Andress-Mallea@users.noreply.github.com>
2026-03-23 08:14:04 -0400

    Delete src directory

--- src/utils/DateUtils.js
+++
@@ -1,7 +0,0 @@
-export const DateUtils = {
-    formatToInput(dateString) {
-        const date = new Date(dateString);
-        const tzOffset = date.getTimezoneOffset() * 60000;
-        return new Date(date - tzOffset).toISOString().slice(0, 16);
-    }
-};
